a38ba75de747e506b006aaf920c480fbb5c53de7
luis-codex <luisprograming0@gmail.com>
2025-05-07 13:35:49 -0500

    feature: Update release branch to main and improve tsconfig configuration

--- src/core/vite/types.d.ts
+++ src/core/vite/types.d.ts
@@ -1,5 +1,2 @@
-import { type PluginOption } from 'vite';
-
 export type Manifest = chrome.runtime.ManifestV3 | chrome.runtime.ManifestV2;
 export type Props = Manifest | (() => Manifest) | (() => Promise<Manifest>);
-export type FunctionType = (props: Props, output?: string) => PluginOption[];
